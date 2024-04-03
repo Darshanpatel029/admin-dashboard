5259f5605520c71420251c841cc1a46ad27efab7
Darshanpatel029 <darshanpatel34319924@gmail.com>
2024-04-03 17:57:28 +0530

    add forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import Main from './Components/Main';
+import Home from './Components/MainContent/Home';
+import Enquiry from './Components/MainContent/Enquiry';
+import DetailEnquiry from './Components/MainContent/DetailEnquiry';
+import Assesments from './Components/MainContent/Assesments';
+import Application from './Components/MainContent/Application';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Components/LogIn/Login';
+import Register from './Components/Register/Register';
+import Profile from './Components/Profile/Profile';
+import Error from './Components/Error/Error';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Enquiry" element={<Enquiry />} />
+        <Route path="/DetailEnquiry" element={<DetailEnquiry />} />
+        <Route path="/Assesments" element={<Assesments />} />
+        <Route path="/Application" element={<Application />} />
+        <Route path="/LogIn" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="*" element={<Error />} />
+
       </Routes>
     </Router>
 
---
+++ src/Components/MainContent/Assesments.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import NavBar from '../Navbar/NavBar';
+import SideBar from '../Sidebar/SideBar';
+import Footer from '../Footer/Footer';
+
+const Assesments = () => {
+    return (
+        <>
+            <NavBar />
+            <SideBar />
+
+            {/* <Footer /> */}
+        </>
+    )
+}
+
+export default Assesments;
