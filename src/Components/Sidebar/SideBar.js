b1c058487fb1fee4e926c0c97fc4e60e0421acbc
Darshanpatel029 <darshanpatel34319924@gmail.com>
2024-04-12 18:09:10 +0530

    add api of add enquiry

--- src/Components/Sidebar/SideBar.js
+++ src/Components/Sidebar/SideBar.js
@@ -8,14 +8,14 @@ const SideBar = () => {
             <ul className="sidebar-nav" id="sidebar-nav">
 
                 <li className="nav-item">
-                    <Link className="nav-link" to="/ViewEnquiry" data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard">
+                    <Link className="nav-link collapsed" to="/ViewEnquiry" data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard">
                         <i className="bi bi-grid" ></i>
                         <span>Dashboard</span>
                     </Link>
                 </li>
 
                 <li className="nav-item">
-                    <Link className="nav-link " to="/ViewDetailEnquiry" data-bs-toggle="tooltip" data-bs-placement="right" title="Detail-Enquiry">
+                    <Link className="nav-link collapsed" to="/ViewDetailEnquiry" data-bs-toggle="tooltip" data-bs-placement="right" title="Detail-Enquiry">
                         <i className="bi bi-bar-chart"></i>
                         <span>Detail-Enquiry</span>
 
@@ -23,22 +23,22 @@ const SideBar = () => {
                 </li>
 
                 <li className="nav-item">
-                    <Link className="nav-link " to="/ViewAssessments" data-bs-toggle="tooltip" data-bs-placement="right" title="Assessments">
+                    <Link className="nav-link collapsed" to="/ViewAssessments" data-bs-toggle="tooltip" data-bs-placement="right" title="Assessments">
                         <i className="bi bi-file-earmark-fill"></i>
                         <span>Assessments</span>
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link " to="/ViewApplication" data-bs-toggle="tooltip" data-bs-placement="right" title="Application">
+                    <Link className="nav-link collapsed " to="/ViewApplication" data-bs-toggle="tooltip" data-bs-placement="right" title="Application">
                         <i className="bi bi-window-stack"></i>
                         <span>Application</span>
                     </Link>
                 </li>
 
                 <li className="nav-item">
-                    <Link to="/LogIn" className="nav-link collapsed" data-bs-toggle="tooltip" data-bs-placement="right" title="Login" >
+                    <Link to="/" className="nav-link collapsed" data-bs-toggle="tooltip" data-bs-placement="right" title="Login" >
                         <i className="bi bi-box-arrow-in-right"></i>
-                        <span>Login</span>
+                        <span>Sign Out</span>
                     </Link>
                 </li>
             </ul>
