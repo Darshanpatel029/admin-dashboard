4be2242f836a3125c6aacda2f42bb2675b95cfa2
Darshanpatel029 <darshanpatel34319924@gmail.com>
2024-06-07 12:19:07 +0530

    clear token function add when i click on signout

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -10,6 +10,10 @@ const NavBar = () => {
         document.body.classList.toggle("toggle-sidebar");
     };
 
+    const handleSignOut = () => {
+        localStorage.removeItem('token');
+    }
+
     return (
         <header id="header" className="header fixed-top d-flex align-items-center">
             <div className="d-flex align-items-center justify-content-between">
@@ -42,8 +46,9 @@ const NavBar = () => {
                         <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                             <li>
                                 <Link
-                                    className="dropdown-item d-flex align-items-center"
                                     to="/"
+                                    onClick={handleSignOut}
+                                    className="dropdown-item d-flex align-items-center"
                                 >
                                     <i className="bi bi-box-arrow-right"></i>
                                     <span>Sign Out</span>
